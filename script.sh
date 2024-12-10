7f216308dd0715d28185692a10f0416870deb490
Kou-Yeager <wongical552@gmail.com>
2024-12-10 12:04:51 +0700

    script : MistOS -> LineageOS

--- script.sh
+++ script.sh
@@ -9,13 +9,10 @@ rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
-
-# remove build/song
-rm -rf build/song
 
 # cloning device tree
 git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b lineage-22 device/xiaomi/mojito
@@ -26,14 +23,8 @@ git clone https://github.com/pyo3377/kxm --depth 1 -b inline-rom kernel/xiaomi/m
 # cloning vendor tree
 git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
 
-# add build/song new
-git clone https://github.com/Kou-Yeager/build_soong.git --depth 1 -b 15 build/song
-
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-mistify mojito userdebug
-
-# Build the ROM (use mka bacon for a full build)
-mist b
+brunch mojito userdebug
