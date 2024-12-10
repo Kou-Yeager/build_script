129236f5c8b4d5894dc78ac8836fac65fa85a7a1
Kou-Yeager <wongical552@gmail.com>
2024-12-10 09:48:31 +0700

    script : add new build song

--- script.sh
+++ script.sh
@@ -14,6 +14,9 @@ repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
+# remove build/song
+rm -rf build/song
+
 # cloning device tree
 git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b lineage-22 device/xiaomi/mojito
 
@@ -22,6 +25,9 @@ git clone https://github.com/pyo3377/kxm --depth 1 -b inline-rom kernel/xiaomi/m
 
 # cloning vendor tree
 git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
+
+# add build/song new
+git clone https://github.com/Kou-Yeager/build_soong.git --depth 1 -b 15 build/song
 
 # Set up th build environment
 . build/envsetup.sh
