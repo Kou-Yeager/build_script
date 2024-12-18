dab3ed8b011bb5a9d8185335248dc974bc5541d0
Kou-Yeager <wongical552@gmail.com>
2024-12-18 20:22:21 +0700

    script : lineage OS -> 2by2 OS

--- script.sh
+++ script.sh
@@ -9,19 +9,22 @@ rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.0 --git-lfs
+repo init -u https://github.com/2by2-Project/android.git -b 15 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b lineage-22 device/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b 2by2 device/xiaomi/mojito
 
 # cloning kernel tree
 git clone https://github.com/Kou-Yeager/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
 git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
+
+# add leica
+git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito-leicacamera.git -b main vendor/xiaomi/mojito-leicacamera
 
 # Set up th build environment
 . build/envsetup.sh
