d5678f2106f1d3e74029c4b0f88629048b59cae8
Kou-Yeager <wongical552@gmail.com>
2024-12-25 12:17:45 +0700

    script : welcome derpFest

--- script.sh
+++ script.sh
@@ -9,13 +9,13 @@ rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b lineage-22 device/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b dpf device/xiaomi/mojito
 
 # cloning kernel tree
 git clone https://github.com/Kou-Yeager/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
@@ -23,11 +23,11 @@ git clone https://github.com/Kou-Yeager/android_kernel_xiaomi_mojito.git --depth
 # cloning vendor tree
 git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
 
-# add leica
-git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito-leicacamera.git -b main vendor/xiaomi/mojito-leicacamera
-
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-brunch mojito userdebug
+lunch derp_mojito-userdebug
+
+# full building
+mka derp
