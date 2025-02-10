262952c34e14ab53854fdb016a6966b1470175d4
Kou-Yeager <wongical552@gmail.com>
2025-02-10 20:35:02 +0700

    test build

--- script.sh
+++ script.sh
@@ -15,13 +15,13 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito.git --depth 1 -b lineage-22.1 device/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/device_xiaomi_mojito.git --depth 1 -b A15 device/xiaomi/mojito
 
 # cloning kernel tree
-git clone https://github.com/Kou-Yeager/kernel_xiaomi_mojito.git --depth 1 -b BORE+UCLAMP kernel/xiaomi/mojito
+git clone https://github.com/BlissRoms-Devices/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
-git clone https://github.com/Kou-Yeager/android_vendor_xiaomi_mojito.git --depth 1 -b lineage-22 vendor/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/vendor_xiaomi_mojito.git --depth 1 -b A15 vendor/xiaomi/mojito
 
 # Set up th build environment
 . build/envsetup.sh
