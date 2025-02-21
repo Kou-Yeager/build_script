71538ea364bb8466d94816c133b5c84d1069765f
Kou-Yeager <wongical552@gmail.com>
2025-02-21 21:15:49 +0700

    test build

--- script.sh
+++ script.sh
@@ -9,22 +9,25 @@ rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs
+repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # cloning device tree
-git clone https://github.com/Kou-Yeager/device_xiaomi_mojito.git --depth 1 -b A15 device/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/device_xiaomi_mojito.git --depth 1 -b 13 device/xiaomi/mojito
 
 # cloning kernel tree
-git clone https://github.com/BlissRoms-Devices/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/OLIVE-SYX/kernel_xiaomi_mojito.git --depth 1 -b 13 kernel/xiaomi/mojito
 
 # cloning vendor tree
-git clone https://github.com/Kou-Yeager/vendor_xiaomi_mojito.git --depth 1 -b A15 vendor/xiaomi/mojito
+git clone https://github.com/OLIVE-SYX/vendor_xiaomi_mojito.git --depth 1 -b 13 vendor/xiaomi/mojito
 
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-brunch mojito userdebug
+lunch aosp_mojito-userdebug
+
+# Build the code
+mka bacon
