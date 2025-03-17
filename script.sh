c24d61cd2f79e82684e15b8f7e432c6381e05f29
Kou-Yeager <wongical552@gmail.com>
2025-03-17 12:32:15 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,29 +5,48 @@ rm -rf .repo/local_manifests/
 
 # remove device tree
 rm -rf device/xiaomi/mojito
+rm -rf device/xiaomi/sm6150-common
 rm -rf kernel/xiaomi/mojito
-rm -rf vendor/xiaomi/mojito
+rm -rf vendor/xiaomi/sm6150-common
+rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
+# remove frameworks/native
+ rm -rf frameworks/native
+
 # cloning device tree
-git clone https://github.com/Kou-Yeager/device_xiaomi_mojito.git --depth 1 -b 13 device/xiaomi/mojito
+git clone https://github.com/zeydann/android_device_xiaomi_mojito.git --depth 1 -b axion device/xiaomi/mojito
+git clone https://github.com/zeydann/android_device_xiaomi_sm6150-common.git --depth 1 -b axion device/xiaomi/sm6150-common
 
 # cloning kernel tree
-git clone https://github.com/OLIVE-SYX/kernel_xiaomi_mojito.git --depth 1 -b 13 kernel/xiaomi/mojito
+git clone https://github.com/zeydann/kernel_xiaomi_mojito.git --depth 1 -b 15 kernel/xiaomi/mojito
 
 # cloning vendor tree
-git clone https://github.com/OLIVE-SYX/vendor_xiaomi_mojito.git --depth 1 -b 13 vendor/xiaomi/mojito
+git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 1 -b 15 vendor/xiaomi/mojito
+git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 15 vendor/xiaomi/sm6150-common
+
+# cloning hardware tree
+git clone https://github.com/zeydann/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+
+# add modify
+ git clone https://github.com/Kou-Yeager/android_frameworks_native.git --depth 1 -b 15 frameworks/native
+
+# Export
+export SELINUX_IGNORE_NEVERALLOWS=true
 
 # Set up th build environment
 . build/envsetup.sh
 
-# Choose the target device
-lunch aosp_mojito-userdebug
+# keys
+gk -s
 
-# Build the code
-mka bacon
+# Choose the target device
+axion mojito va
+
+# full target
+brunch mojito userdebug
