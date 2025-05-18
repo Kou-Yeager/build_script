c6cf63b9fb41ea8ce81985962f3b0e0e37c8b34d
Kou-Yeager <wongical552@gmail.com>
2025-05-18 14:26:17 +0700

    test

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@
 rm -rf .repo/local_manifests/
 
 # Initialize ROM manifest
-repo init -u https://github.com/zeydann/android-cr.git -b 13.0 --git-lfs
+repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
 # Device tree
-git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito-13.git -b 13 device/xiaomi/mojito
+git clone https://github.com/Kou-Yeager/android_device_xiaomi_mojito-13.git -b pe device/xiaomi/mojito
 
 # Kernel tree
 git clone https://github.com/device-xti/android_kernel_xiaomi_mojito.git -b thirteen kernel/xiaomi/mojito
@@ -22,7 +22,7 @@ git clone https://github.com/FerryAr/vendor-xiaomi-mojito.git -b thirteen vendor
 . build/envsetup.sh
 
 # Choose the target device
-lunch lineage_mojito-userdebug
+lunch aosp_mojito-userdebug
 
 # full target
-m bacon
+mka bacon
