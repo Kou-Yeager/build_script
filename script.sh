232490c8dbc473c15dae204507cec81e51d5f051
ZaidanAgusta <zaidanagusta319@gmail.com>
2025-05-22 07:48:06 +0700

    test2

--- script.sh
+++ script.sh
@@ -1,25 +1,30 @@
+
 #!/bin/bash
 
 # Remove the local manifests directory if it exists (cleanup before repo initialization)
 rm -rf .repo/local_manifests/
+rm -rf device/xiaomi/mojito
 
 # Initialize ROM manifest
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+#repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
-# remove frameworks/native
-rm -rf frameworks/native
+# Device tree
+git clone https://github.com/Zeydann/device_xiaomi_mojito-13.git -b 13 device/xiaomi/mojito
 
-# add frameworks/native
-git clone https://github.com/zeydann/frameworks_native-axion.git -b 15 frameworks/native
+# Kernel tree
+#git clone https://github.com/device-xti/android_kernel_xiaomi_mojito.git -b thirteen kernel/xiaomi/mojito
+
+# Vendor tree
+#git clone https://github.com/FerryAr/vendor-xiaomi-mojito.git -b thirteen vendor/xiaomi/mojito
 
 # Set up th build environment
 . build/envsetup.sh
 
 # Choose the target device
-axion mojito va
+lunch aosp_mojito-userdebug
 
 # full target
-brunch mojito user
+mka bacon
