17b1a7785f7e3e690d4eedff9d6912fd1942901a
ZaidanAgusta <zaidanagusta319@gmail.com>
2025-06-14 06:41:56 +0700

    test3444

--- script.sh
+++ script.sh
@@ -5,16 +5,16 @@
 rm -rf .repo/local_manifests/
 
 # Initialize ROM manifest
-repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
+#repo init -u https://github.com/PixelExperience-LEGACY-edition/manifest.git -b thirteen-plus --depth=1 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # remove fwb
 rm -rf frameworks/base
 
 # add fwb
-git clone https://github.com/Zeydann/frameworks_base-pe.git --depth=1 -b 13 frameworks/base
+git clone https://github.com/Zeydann/frameworks_base-pe.git --depth=1 -b wip frameworks/base
 
 # Device tree
 #git clone https://github.com/Zeydann/device_xiaomi_mojito-13.git -b pe device/xiaomi/mojito
@@ -26,7 +26,7 @@ git clone https://github.com/Zeydann/frameworks_base-pe.git --depth=1 -b 13 fram
 #git clone https://github.com/FerryAr/vendor-xiaomi-mojito.git -b thirteen vendor/xiaomi/mojito
 
 # MiuiCamera
-git clone https://gitlab.com/Sepidermn/vendor-xiaomi-mojito-miuicamera.git -b thirteen vendor/xiaomi/mojito-miuicamera
+#git clone https://gitlab.com/Sepidermn/vendor-xiaomi-mojito-miuicamera.git -b thirteen vendor/xiaomi/mojito-miuicamera
 
 # Set up th build environment
 . build/envsetup.sh
